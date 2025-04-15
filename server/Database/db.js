8244ef41926954c6347abff3c5320065f07deab7
Rahilkhan5210 <rahilkhan5210@gmail.com>
2025-04-15 15:56:48 +0530

    modify

--- server/Database/db.js
+++ server/Database/db.js
@@ -6,6 +6,7 @@ const connectDB = async()=>{
         console.log("Database connected");
     } catch (error) {
         console.log("Database connection error", error);
+        process.exit(1);
     }
 }
 export default connectDB;
